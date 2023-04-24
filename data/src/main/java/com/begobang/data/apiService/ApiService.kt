8ae19bc4b6772bf9d1404a27eb5996157ad60303
begobang <106311072+begobang@users.noreply.github.com>
2023-04-24 20:13:12 +0200

    [FEATURE] ApiService extension functions implemented.

---
+++ data/src/main/java/com/begobang/data/apiService/ApiService.kt
@@ -0,0 +1,41 @@
+package com.begobang.data.apiService
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.begobang.domain.failure.Failure
+import retrofit2.Call
+import retrofit2.Response
+
+suspend fun <Api, Domain> requestGenericApi(
+    call: Call<Api>,
+    success: (Api?) -> Domain?
+): Either<Failure, Domain?> {
+    return Either.catch {
+        call
+    }.fold(
+        { error ->
+            manageException(error)
+        },
+        { response ->
+            val res = response.execute()
+            parseGenericResponse(res, success)
+        }
+    )
+
+}
+
+private fun <Domain> manageException(error: Throwable): Either<Failure, Domain> {
+    return Failure.BaseFailure(message = error.message).left()
+}
+
+private fun <Api, Domain> parseGenericResponse(
+    response: Response<Api>,
+    success: (Api?) -> Domain
+): Either<Failure, Domain> {
+    return if(response.isSuccessful) {
+        success(response.body()).right()
+    } else {
+        Either.left(Failure.BaseFailure("error"))
+    }
+}
