60e2b9292f99ccc66e4b4133f0fab118239ff913
bfernave <begona.fernandezvelasco@nttdata.com>
2023-04-25 10:06:29 +0200

    [REFACTOR] limit query param added to GetPokemonApiService.

--- data/src/main/java/com/begobang/data/apiService/GetPokemonsApiService.kt
+++ data/src/main/java/com/begobang/data/apiService/GetPokemonsApiService.kt
@@ -3,8 +3,11 @@ package com.begobang.data.apiService
 import com.begobang.data.response.PokemonsResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface GetPokemonsApiService {
     @GET("pokemon")
-    fun getPokemons(): Call<PokemonsResponse>
+    fun getPokemons(
+        @Query("limit") limit: Int = 80
+    ): Call<PokemonsResponse>
 }
