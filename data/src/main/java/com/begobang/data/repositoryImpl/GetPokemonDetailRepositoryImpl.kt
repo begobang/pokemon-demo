6eb1671850fd8eaece9e9b868c6cb2d296c89892
bfernave <begona.fernandezvelasco@nttdata.com>
2023-04-25 16:18:26 +0200

    [WIP] GetPokemonDetailRepositoryImpl implemented.

--- data/src/main/java/com/begobang/data/repositoryImpl/GetPokemonDetailRepositoryImpl.kt
+++ data/src/main/java/com/begobang/data/repositoryImpl/GetPokemonDetailRepositoryImpl.kt
@@ -2,11 +2,8 @@ package com.begobang.data.repositoryImpl
 
 import arrow.core.Either
 import com.begobang.data.remoteDataSource.GetPokemonDetailRemoteDataSource
-import com.begobang.data.remoteDataSource.GetPokemonsRemoteDataSource
 import com.begobang.domain.GetPokemonDetailRepository
-import com.begobang.domain.GetPokemonsRepository
 import com.begobang.domain.business.PokemonDetailBusiness
-import com.begobang.domain.business.PokemonsBusiness
 import com.begobang.domain.failure.Failure
 import javax.inject.Inject
 
@@ -14,7 +11,13 @@ class GetPokemonDetailRepositoryImpl @Inject constructor(
     private val pokemonDetailRemoteDataSource: GetPokemonDetailRemoteDataSource
 ): GetPokemonDetailRepository {
     override suspend fun getPokemonDetail(name: String): Either<Failure, PokemonDetailBusiness?> {
-        return pokemonDetailRemoteDataSource.getPokemonDetail(name)
+        return try {
+            when (val response = pokemonDetailRemoteDataSource.getPokemonDetail(name)) {
+                is Either -> response
+            }
+        } catch (e: Exception){
+            Either.Left(Failure.BaseFailure(message = e.localizedMessage))
+        }
     }
 
 }
